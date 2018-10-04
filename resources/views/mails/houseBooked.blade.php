1198a1a23533ef5bfa56a1e036380933687052a8
Alliance Peace <peaceprivate@outlook.com>
2018-10-04 18:18:44 +0200

    added the ability to send emails after booking a house

---
+++ resources/views/mails/houseBooked.blade.php
@@ -0,0 +1,7 @@
+Hello Dear Sir/Madam,
+<p>House booking has been successful. Click on the link below to view house details.</p>
+<p><a href="{{ $link }}" target="_blank">View house details</a></p>
+<p>Thank you for working with us.</p>
+<p>Best Regards.</p>
+<br/>
+<p>Icumbi Team</p>
