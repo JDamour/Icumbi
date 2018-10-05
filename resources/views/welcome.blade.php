ecf6112fb9b5a7c522d012a1b4c0d89b765d5e9d
Marie Bertrande UMUHIRE <mariebertrande.umuhire@gmail.com>
2018-10-05 19:08:48 +0200

    removed some unnecessary content from the home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -385,7 +385,7 @@
 
   <!-- END: section -->
 
-  <section class="probootstrap-section probootstrap-section-lighter">
+ <!--  <section class="probootstrap-section probootstrap-section-lighter">
     <div class="container">
       <div class="row heading">
         <h2 class="mt0 mb50 text-center">Our Agents</h2>
@@ -441,7 +441,7 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> -->
 
   <footer class="probootstrap-footer probootstrap-bg" style="background-image: url(img/slider_3.jpg)">
     <div class="container">
