4e0fd07c38dea844e339df81d32513849a55096a
Marie Bertrande UMUHIRE <mariebertrande.umuhire@gmail.com>
2018-11-03 21:51:25 +0200

    changes

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -2,56 +2,40 @@
 @extends('layouts.app')
 
 
-  <section class="probootstrap-slider flexslider2 page-inner">
-    <div class="overlay"></div>
-    <div class="probootstrap-wrap-banner">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8">
-            <div class="page-title probootstrap-animate">
-              <div class="probootstrap-breadcrumbs">
-                <a href="/">Home</a><span>house</span>
-              </div>
-              <h1>Houses</h1>
+<section class="probootstrap-slider flexslider2 page-inner">
+  <div class="overlay"></div>
+  <div class="probootstrap-wrap-banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="page-title probootstrap-animate">
+            <div class="probootstrap-breadcrumbs">
+              <a href="/">Home</a><span>house</span>
             </div>
+            <h1>Houses</h1>
           </div>
         </div>
       </div>
     </div>
-  </section>
-  
-  <section class="probootstrap-section probootstrap-section-lighter">
-    <div class="container">
-      <div class="row">
+  </div>
+</section>
+
+<section class="probootstrap-section probootstrap-section-lighter">
+  <div class="container">
+    <div class="row">
       @foreach($houses as $house)
-        <div class="col-md-4 col-sm-6">
-          <div class="probootstrap-card probootstrap-listing">
-            <div class="probootstrap-card-media">
+      <div class="col-md-4 col-sm-6">
+        <div class="probootstrap-card probootstrap-listing">
+          <div class="probootstrap-card-media">
             <a href="{{route('houseshow.show', $house->id)}}">
-    
+
               @foreach($house->uploads as $upload)
-                <img src="/images/HouseUploads/{{ $upload->source }}">
-                @break;
+              <img src="/images/HouseUploads/{{ $upload->source }}">
+              @break;
               @endforeach
 
             </div></a>
-            <div class="probootstrap-card-text">
-            <!-- <h2 class="probootstrap-card-heading"><a href="{{route('houseshow.show', $house->id)}}">House Id: {{ $house->id }}</a></h2> -->
-              <div class="probootstrap-listing-location">
-              <h2 class="probootstrap-card-heading"><a href="{{route('houseshow.show', $house->id)}}">Number of Bed rooms: {{ $house->numberOfRooms }}</a></h2>
-              <a href="{{route('houseshow.show', $house->id)}}">  <i class="icon-location2"></i> <span>Location:   {{ $house->sector['name'] }}/{{ $house->district['name'] }}</span><br/>
-              </div>
-              <div class="probootstrap-listing-category for-sale"><span>For Rent</span></div>
-              <div class="probootstrap-listing-price"><strong>Price:{{ $house->housePrice }} {{ $house->paymentfrequency['name'] }}</strong></div>
-            </div></a>
-             
-            
-            <div class="probootstrap-card-extra">
-            </div>
-          </div>
-        </div>
-        @endforeach
-      </div>
-      {{ $houses->links() }}
-    </div>
-  </section>
+
+           
+    </section>
+
