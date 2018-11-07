3a9d6a8023b9e0819968f3052055afc8b3a6e610
Alliance Peace <peaceprivate@outlook.com>
2018-11-07 09:49:02 +0200

    added user management routes

--- routes/web.php
+++ routes/web.php
@@ -91,7 +91,14 @@ Route::group(['prefix' => 'admin', 'middleware' =>'auth.admin'],function () {
     Route::post('/uploads', 'AdminUploadsController@store')->name('admin.uploads.store') ;
     Route::delete('/uploads/{id}', 'AdminUploadsController@destroy')->name('admin.uploads.delete') ;
 
-    Route::resource('/','UserManagementController');
+    // user management controller
+    Route::get('/users', 'UserManagementController@index');
+    Route::get('/users/create', 'UserManagementController@create');
+    Route::get('/users/{id}', 'UserManagementController@show');
+    Route::get('/users/{id}/edit', 'UserManagementController@edit');
+    Route::post('/users', 'UserManagementController@store');
+    Route::put('/users/{id}', 'UserManagementController@update');
+    Route::delete('/users/{id}', 'UserManagementController@destroy');
 });
 
 # houseOwner routes
